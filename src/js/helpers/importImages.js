b0e15a43e7ef9b66e58d70aba41d52877311f559
Alfy Hushairi <alfy.hushairi@accedo.tv>
2020-10-02 17:11:06 +0100

    Added-portfolio page and styling

--- src/js/helpers/importImages.js
+++ src/js/helpers/importImages.js
@@ -10,6 +10,16 @@ import rabbit_desktop from "../../assets/rabbit_desktop.png";
 import rabbit_mobile from "../../assets/rabbit_mobile.png";
 import me_bg_desktop from "../../assets/me_bg_desktop.png";
 
+import image_one from "../../assets/image_one.png";
+import image_two from "../../assets/image_two.png";
+import image_three from "../../assets/image_three.png";
+import image_four from "../../assets/image_four.png";
+import image_five from "../../assets/image_five.png";
+import image_six from "../../assets/image_six.png";
+import image_seven from "../../assets/image_seven.png";
+import image_eight from "../../assets/image_eight.png";
+
+
 const images = {
   github_focused,
   github_unfocused,
@@ -21,7 +31,15 @@ const images = {
   play_unfocused,
   rabbit_desktop,
   rabbit_mobile,
-  me_bg_desktop
+  me_bg_desktop,
+  image_one,
+  image_two,
+  image_three,
+  image_four,
+  image_five,
+  image_six,
+  image_seven,
+  image_eight
 };
 
 export default images;
