ca9081bb03d741f40cb8a07cfedcbcd073b9f2a5
Alfy Hushairi <alfy.hushairi@accedo.tv>
2022-02-16 15:58:46 +0000

    Updates

---
+++ precache-manifest.e020b40bb07698f827f5fcbddf5ffe91.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8677345f7974afbd2700f7b62b8127e",
+    "url": "/new-portfolio/index.html"
+  },
+  {
+    "revision": "055d5221932ec44b1f3c",
+    "url": "/new-portfolio/static/css/main.29ff1ac0.chunk.css"
+  },
+  {
+    "revision": "a1c5056f299a7c67d116",
+    "url": "/new-portfolio/static/js/2.079fff74.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/new-portfolio/static/js/2.079fff74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "055d5221932ec44b1f3c",
+    "url": "/new-portfolio/static/js/main.05af7503.chunk.js"
+  },
+  {
+    "revision": "479325996ac7fdb04c9c",
+    "url": "/new-portfolio/static/js/runtime-main.6c8eeb54.js"
+  },
+  {
+    "revision": "a963f8e96378e6d9dfaeeba78cefff68",
+    "url": "/new-portfolio/static/media/image_eight.a963f8e9.png"
+  },
+  {
+    "revision": "939184876ffa0fc5180c97287d142cab",
+    "url": "/new-portfolio/static/media/image_five.93918487.png"
+  },
+  {
+    "revision": "69c35df7bd07f87a465a23f33d12812e",
+    "url": "/new-portfolio/static/media/image_four.69c35df7.png"
+  },
+  {
+    "revision": "e4e8d4b6853dd64358e876f7bb73dcbf",
+    "url": "/new-portfolio/static/media/image_one.e4e8d4b6.png"
+  },
+  {
+    "revision": "7096102856f6d7c8c6fa6009b16dca14",
+    "url": "/new-portfolio/static/media/image_seven.70961028.png"
+  },
+  {
+    "revision": "6c14760b06bcd271cd3955624b076973",
+    "url": "/new-portfolio/static/media/image_six.6c14760b.png"
+  },
+  {
+    "revision": "e0dd554b629b82d1e1737534a51efe6e",
+    "url": "/new-portfolio/static/media/image_three.e0dd554b.png"
+  },
+  {
+    "revision": "f2a949fd999d0f5ed96da06c36960d15",
+    "url": "/new-portfolio/static/media/image_two.f2a949fd.png"
+  },
+  {
+    "revision": "175956dc725f22964bb4001c4b7a0faa",
+    "url": "/new-portfolio/static/media/me_bg_desktop.175956dc.png"
+  },
+  {
+    "revision": "2c164cf6c6f3f25a8a2eae14c7567cf4",
+    "url": "/new-portfolio/static/media/rabbit_mobile.2c164cf6.png"
+  }
+]);
